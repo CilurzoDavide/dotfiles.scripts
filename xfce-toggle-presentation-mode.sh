475c0ee5a005a706c19551778c833d64de4edcba
tobecci <ojinakatochukwu@gmail.com>
2024-08-31 03:24:45 +0100

    update: standard desktop size and login actions on xfce

--- xfce-toggle-presentation-mode.sh
+++ xfce-toggle-presentation-mode.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+lock_file='/home/tobecci/.cache/tobecci/keepalive/presentation.lck'
+
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -T
 
 status=$(xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode)
 
 if [ "$status" = "true" ]; then
+	touch $lock_file
 	notify-send "presentation mode is on"
 else
+	rm $lock_file
 	notify-send "presentation mode is off"
 fi
