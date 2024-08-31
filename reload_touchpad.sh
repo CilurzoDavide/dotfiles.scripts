0eec17a044b31ecf6d5f41817b410a7f74975a4d
tobecci <ojinakatochukwu@gmail.com>
2024-08-31 04:00:02 +0100

    update: (note: bitwarden blocks process, damn electron apps)

--- reload_touchpad.sh
+++ reload_touchpad.sh
@@ -15,10 +15,12 @@ reload_touchpad(){
 /usr/bin/redshift -O 1000 > /dev/null 2>&1 &
 
 # send_notification "reloading touchpad...."
-/usr/bin/notify-send 'reload touchpad script' "reloading touchpad......"
+# /usr/bin/notify-send 'reload touchpad script' "reloading touchpad......"
+
 reload_touchpad
 /usr/bin/redshift -x > /dev/null 2>&1
 # disable middle click
 /usr/bin/xinput set-button-map 10 1 0 3 4 5 6 7
+
 # send_notification "done reloading"
-/usr/bin/notify-send 'reload touchpad script' "completed"
+# /usr/bin/notify-send 'reload touchpad script' "completed"
